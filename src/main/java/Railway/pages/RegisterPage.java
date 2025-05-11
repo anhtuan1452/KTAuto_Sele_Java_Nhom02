e736e552f63eee780d192905290c23603057663f
Thuyendn <thuyennd21@gmail.com>
2025-05-11 19:01:46 +0700

    fix re

--- src/main/java/Railway/pages/RegisterPage.java
+++ src/main/java/Railway/pages/RegisterPage.java
@@ -1,5 +1,7 @@
 package Railway.pages;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.*;
 
 
@@ -10,8 +12,8 @@ public class RegisterPage extends GenetralPage {
     private final By confirmpasswordField = By.id("confirmPassword");
     private final By pidField = By.id("pid");
     private final By btnResgiter = By.xpath("//*[@id=\"RegisterForm\"]/fieldset/p/input");
-    private final By messegerError = By.xpath("//div[@id=\"content\"]/p[contains(@class,'message error')]");
-    private final By messeger = By.xpath("//div[@id=\"content\"]/p");
+    private final By messageBlankPassword = By.xpath("//*[@id=\"RegisterForm\"]/fieldset/ol/li[2]/label[2]");
+    private final By formErrorMessage = By.xpath("//div[@id=\"content\"]/p[2]");
     private final By messageBlankPid = By.xpath("/html/body/div[1]/div[2]/form/fieldset/ol/li[4]/label[2]");
 
 
@@ -32,9 +34,9 @@ public class RegisterPage extends GenetralPage {
         return getElement(btnResgiter);
     }
     protected WebElement Messeger(){
-        return getElement(messeger);
+        return getElement(formErrorMessage);
     }
-    protected WebElement getMessegerError() {return getElement(messegerError);}
+    protected WebElement getMessegerError() {return getElement(messageBlankPassword);}
 
     // method
     public RegisterPage(WebDriver driver){
@@ -67,25 +69,52 @@ public class RegisterPage extends GenetralPage {
         scrollElement(registerButton);
         registerButton.click();
     }
-
-
-    public Boolean MessageBlankPib() {
-        try {
-            WebElement label = getElement(messageBlankPid);
-            WebElement pidInput = getElement(By.id("pid"));
-
-            String errorMessage = label.getText().trim();
-            System.out.println("Error message text: " + errorMessage);
-            if (errorMessage.contains("Invalid ID length") &&
-                    label.getRect().getX() > pidInput.getRect().getX()) {
-                return true;
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("Error elements not found: " + e.getMessage());
-            return false;
+    // Kiểm tra thông báo lỗi trên form
+    public void checkFormErrorMessageDisplayed(String expectedMessage, HomePage homePage, ExtentTest test) {
+        WebElement formMessage = getElement(formErrorMessage);
+        String errorMessage = formMessage.getText().trim();
+        System.out.println("Form Error message text: " + errorMessage);
+        if (errorMessage.contains(expectedMessage)) {
+            test.log(Status.PASS, "Message \"" + expectedMessage + "\" appears above the form.");
+        } else {
+            test.fail("Message \"" + expectedMessage + "\" does not appear above the form.");
+            test.addScreenCaptureFromPath(homePage.takeScreenshot(driver, "TC11"));
         }
-        return false;
+
     }
+
+    // Kiểm tra thông báo lỗi PID
+    public void checkMessageBlankPidDisplayed(String expectedMessage, HomePage homePage, ExtentTest test) {
+        WebElement label = getElement(messageBlankPid);
+        WebElement pidInput = getElement(By.id("pid"));
+        String errorMessage = label.getText().trim();
+        System.out.println("PID Error message text: " + errorMessage);
+        if (errorMessage.contains(expectedMessage) &&
+                label.getRect().getX() > pidInput.getRect().getX()) {
+        } else{
+            test.fail("Error message \"Invalid PID length\" does not appear next to PID field.");
+            test.addScreenCaptureFromPath(homePage.takeScreenshot(driver, "TC11"));
+        }
+    }
+
+    // Kiểm tra thông báo lỗi Password
+    public void checkMessageBlankPasswordDisplayed(String expectedMessage, HomePage homePage, ExtentTest test) {
+            WebElement label = getElement(messageBlankPassword);
+            WebElement passwordInput = getElement(By.id("password"));
+            String errorMessage = label.getText().trim();
+            System.out.println("Password Error message text: " + errorMessage);
+            if (errorMessage.contains(expectedMessage) &&
+                    label.getRect().getX() > passwordInput.getRect().getX()) {
+
+            } else {
+                test.fail("Error message \"Invalid password length\" does not appear next to password field.");
+                test.addScreenCaptureFromPath(homePage.takeScreenshot(driver, "TC11"));
+            }
+    }
+
+
+
+
 
 
 
